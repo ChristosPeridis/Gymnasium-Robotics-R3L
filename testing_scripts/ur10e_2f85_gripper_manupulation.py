98c61ea0fd5b5c94de6a047246dfb6b347552dd7
ChristosPeridis <christosperidis@gmail.com>
2023-12-11 16:25:24 +0000

    Second upload of the gripper manupulation testing in the ur10e_2f85 combo.

--- testing_scripts/ur10e_2f85_gripper_manupulation.py
+++ testing_scripts/ur10e_2f85_gripper_manupulation.py
@@ -17,32 +17,37 @@ renderer = MujocoRenderer(model, data)
 time_step = model.opt.timestep
 
 # Control parameters
-max_control = 255  # Maximum control input
-min_control = 0    # Minimum control input
-duration = 60      # Duration of the script in seconds
+max_control = 255  # Maximum control input for opening
+min_control = 0    # Minimum control input for closing
+duration = 60      # Total duration of the script in seconds
+cycles = 5         # Number of open-close cycles
+cycle_duration = duration / cycles
 
-# Function to smoothly interpolate between min and max control values
-def smooth_control(elapsed_time, duration):
-    # Calculate the proportion of the cycle completed (0 to 1 and back to 0)
-    cycle_progress = (1 - np.cos(2 * np.pi * elapsed_time / duration)) / 2
-
-    # Interpolate control input
-    return min_control + cycle_progress * (max_control - min_control)
+# Actuate the gripper
+def actuate_gripper(control_input):
+    data.ctrl[-1] = control_input
+    mujoco.mj_step(model, data)
 
 # Run the simulation
 start_time = time.time()
 while time.time() - start_time < duration:
     elapsed_time = time.time() - start_time
+    cycle_time = elapsed_time % cycle_duration
 
-    # Calculate smooth control input
-    control_input = smooth_control(elapsed_time, duration/2)  # Dividing by 2 for open-close in half duration
+    # Determine if opening or closing phase
+    if cycle_time < cycle_duration / 2:
+        # Opening phase
+        control_input = max_control
+    else:
+        # Closing phase
+        control_input = min_control
 
     # Actuate the gripper
-    data.ctrl[-1] = control_input
-    mujoco.mj_step(model, data)
+    actuate_gripper(control_input)
 
     # Render the scene using Gymnasium's renderer
     renderer.render(render_mode="human")
+
 
     # Wait for the next time step
     time.sleep(time_step)
